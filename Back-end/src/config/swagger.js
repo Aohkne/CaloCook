46ffe84d65f0ff6f6f62c760130abbdd7a16ecd9
KHOA <aohkne@gmail.com>
2025-07-16 21:56:04 +0700

    fix(develop): done

---
+++ Back-end/src/config/swagger.js
@@ -0,0 +1,32 @@
+import { env } from '@/config/environment'
+
+export const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API',
+      version: '1.0.0',
+      description: 'Management'
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    },
+    servers: [
+      {
+        url: `http://localhost:${env.PORT || 8080}`,
+        description: 'Development server'
+      },
+      {
+        url: 'https://calocook.onrender.com',
+        description: 'Production server'
+      }
+    ]
+  },
+  apis: ['./src/routes/v1/*.js']
+}
---
+++ Back-end/src/server.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-console*/
+
+import express from 'express'
+import cors from 'cors'
+import exitHook from 'async-exit-hook'
+
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import { swaggerOptions } from '@/config/swagger'
+
+import { CONNECT_DB, CLOSE_DB } from '@/config/mongodb'
+import { env } from '@/config/environment'
+
+import { APIs_V1 } from '@/routes/v1'
+import { errorHandlingMiddleware } from '@/middlewares/errorHandlingMiddleware'
+
+const START_SERVER = () => {
+  const app = express()
+  const swaggerSpec = swaggerJSDoc(swaggerOptions)
+
+  // CORS
+  app.use(cors())
+
+  // Enable req.body json data
+  app.use(express.json({ limit: '10mb' }))
+
+  // API V1
+  app.use('/api/v1', APIs_V1)
+
+  // Swagger
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+  // Middleware - xử lý lỗi tập trung
+  app.use(errorHandlingMiddleware)
+
+  if (env.NODE_ENV === 'production') {
+    // PRODUCTION
+    const port = process.env.PORT || env.PORT || 8080
+    app.listen(port, () => {
+      console.log(`Hello ${env.AUTHOR}, Server is running at: https://calocook.onrender.com`)
+      console.log(`Swagger is running at https://calocook.onrender.com/api-docs`)
+    })
+  } else {
+    // LOCAL
+    app.listen(env.PORT, env.HOST, () => {
+      console.log(`Hello ${env.AUTHOR}, Server is running at http://${env.DISPLAY_HOST}:${env.PORT}/`)
+      console.log(`Swagger is running at http://${env.DISPLAY_HOST}:${env.PORT}/api-docs`)
+    })
+  }
+
+  // CLEAN UP
+  exitHook(() => {
+    console.log('Server is shutting down...')
+    CLOSE_DB()
+    console.log('Disconnected from MONGODB')
+  })
+}
+
+;(async () => {
+  try {
+    console.log('Connecting to MONGODB...')
+    await CONNECT_DB()
+
+    console.log('Connected to MONGODB')
+
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
