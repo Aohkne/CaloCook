3cd0fae058b746e26c8d6b62db7d53eca8920aa6
KHOA <aohkne@gmail.com>
2025-07-16 20:44:40 +0700

    fix(Cors): done

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -21,7 +21,7 @@ const START_SERVER = () => {
   // CORS
   app.use(
     cors({
-      origin: ['https://calocook.onrender.com', 'http://localhost:8080'],
+      origin: ['https://calocook.onrender.com', 'http://localhost:8080', 'http://localhost:5173'],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization']
