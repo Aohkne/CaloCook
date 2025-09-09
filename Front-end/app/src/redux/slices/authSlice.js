9381d57061682b7b55ba8fa64dc1ee4c1afba9e9
ChanNG <145467988+ChanPotter@users.noreply.github.com>
2025-09-09 19:16:25 +0700

    feat(fe:forgotPassword):done

--- Front-end/app/src/redux/slices/authSlice.js
+++ Front-end/app/src/redux/slices/authSlice.js
@@ -61,9 +61,9 @@ export const forgotPassword = createAsyncThunk('auth/forgotPassword', async (ema
 
 export const resetPassword = createAsyncThunk(
   'auth/resetPassword',
-  async ({ token, newPassword }, { rejectWithValue }) => {
+  async ({ otp, email, password }, { rejectWithValue }) => {
     try {
-      const response = await resetPasswordService(token, newPassword);
+      const response = await resetPasswordService(otp, email, password);
       return response;
     } catch (error) {
       return rejectWithValue(error);
