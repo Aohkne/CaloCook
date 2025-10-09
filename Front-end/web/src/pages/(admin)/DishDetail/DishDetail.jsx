1ad8b4f7d8622e49a5bb32bc2a875e02820ce99c
Nguyen Trong Quy <trongquy131204@gmail.com>
2025-10-09 12:26:18 +0700

    feat(Dish Detail UI): 30%

--- Front-end/web/src/pages/(admin)/DishDetail/DishDetail.jsx
+++ Front-end/web/src/pages/(admin)/DishDetail/DishDetail.jsx
@@ -40,29 +40,56 @@ function DishDetail() {
   if (loading) return <div className={cx('wrapper')}>Loading...</div>;
   if (error) return <div className={cx('wrapper', 'error')}>{error}</div>;
   if (!dish) return <div className={cx('wrapper')}>No dish found</div>;
+  console.log(dish.imageUrl);
 
   return (
     <div className={cx('wrapper')}>
-      <div className={cx('back')}>
+      {/* Back Button */}
+      <div className={cx('back-button')}>
         <Link to={'/dish-management'} className={cx('back-link')}>
-          <Icon icon='ic:round-arrow-back' /> Back to list
+          <Icon icon='formkit:left' width='7' height='16' /> Back to list
         </Link>
       </div>
-
-      <div className={cx('card')}>
-        <div className={cx('image')}>
-          {dish.imageUrl ? (
-            <img src={dish.imageUrl} alt={dish.name} />
-          ) : (
-            <img src='/images/default-img.png' alt='default' />
-          )}
+      {/* Main Container */}
+      <div className={cx('container')}>
+        {/* Dish Image */}
+        <div className={cx('dish-image-container')}>
+          <img className={cx('dish-image')} src={'/img/hawaiian-chicken-salad.png'} alt={dish.name} />
         </div>
-        <div className={cx('content')}>
-          <h2 className={cx('title')}>{dish.name}</h2>
-          <p className={cx('meta')}>Cooking time: {dish.cookingTime || 'N/A'} min</p>
-          <p className={cx('meta')}>Calories: {dish.calorie || 'N/A'}</p>
-          <p className={cx('meta')}>Difficulty: {dish.difficulty || 'N/A'}</p>
-          <div className={cx('description')}>{dish.description || 'No description'}</div>
+        <div className={cx('dish-infomation')}>
+          {/* Dish Name */}
+          <h1 className={cx('dish-name')}>{dish.name}</h1>
+          {/* Dish Description */}
+          <p className={cx('dish-description')}>{dish.description}</p>
+          {/* Time, Calories, Difficulty Level Cards */}
+          <div className={cx('time-colories-difficulty-container')}>
+            <div className={cx('card')}>
+              <Icon icon='basil:clock-outline' width='24' height='24' className={cx('card-icon')} />
+              <div className={cx('card-text-container')}>
+                <p className={cx('card-title')}>Cooking Time</p>
+                <p className={cx('card-value')}>{dish.cookingTime} Minutes</p>
+              </div>
+            </div>
+            <div className={cx('card')}>
+              <Icon icon='basil:fire-outline' width='24' height='24' className={cx('card-icon')} />
+              <div className={cx('card-text-container')}>
+                <p className={cx('card-title')}>Calories</p>
+                <p className={cx('card-value')}>{dish.calorie} Kcal</p>
+              </div>
+            </div>
+            <div className={cx('card', 'span-2')}>
+              <Icon icon='lucide-lab:hat-chef' width='24' height='24' className={cx('card-icon')} />
+              <div className={cx('card-text-container')}>
+                <p className={cx('card-title')}>Difficulty Level</p>
+                <p className={cx('card-value')}>{dish.difficulty}</p>
+              </div>
+            </div>
+            {/* Edit Button */}
+            <button className={cx('edit-button', 'span-2')}>
+              <Icon icon='lucide:pen' width='24' height='24' />
+              Edit Dish
+            </button>
+          </div>
         </div>
       </div>
     </div>
