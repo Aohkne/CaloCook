787f8f7db00f35d3addfbd318c8c26bb06dca769
KHOA <aohkne@gmail.com>
2025-10-30 14:22:48 +0700

    feat(calobot-ai): integrate app

--- Front-end/app/src/services/api_ai.js
+++ Front-end/app/src/services/api_ai.js
@@ -4,8 +4,7 @@ import { AI_API_URL } from '@env';
 console.log('Fe get AI API at:' + AI_API_URL);
 const api = axios.create({
   baseURL: AI_API_URL,
-  timeout: 10000,
-  withCredentials: false
+  timeout: 30000
 });
 
 export default api;
